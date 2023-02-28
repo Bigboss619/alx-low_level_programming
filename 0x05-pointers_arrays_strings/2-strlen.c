1065830528dd1f022934a8ada290fbedae6db329
Bigboss619 <eugochukwu910@gmail.com>
2023-02-28 02:33:01 -0800

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,14 @@
 /**
  * _strlen - Returns the length of a string.
  * @str: The string to get the length of
- * Retrun: The length of @str.
- */
+ * Return: The length of @str.
+ *
+*/
 size_t _strlen(const char *str)
 {
 	size_t length = 0;
 
-	while(*str++)
+	while (*str++)
 	length++;
 	return (length);
 }
